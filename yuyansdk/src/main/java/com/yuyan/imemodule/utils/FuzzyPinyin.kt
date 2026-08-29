6431ddffe8c1de941f16c5aee42b9299fd91a800
Xuanyze <Xuanyze@users.noreply.github.com>
2026-08-29 23:36:29 +0800

    修复 FuzzyPinyin Kotlin 字符串 $ 转义

--- yuyansdk/src/main/java/com/yuyan/imemodule/utils/FuzzyPinyin.kt
+++ yuyansdk/src/main/java/com/yuyan/imemodule/utils/FuzzyPinyin.kt
@@ -12,12 +12,12 @@ import java.io.File
 object FuzzyPinyin {
     // id to (标题, derive 规则列表)
     val GROUPS: Map<String, Pair<String, List<String>>> = mapOf(
-        "zh_z" to ("模糊音 zh/z" to listOf("derive/^([zcs])h/$1/", "derive/^([zcs])([aeiou])/$1h$2/")),
+        "zh_z" to ("模糊音 zh/z" to listOf("derive/^([zcs])h/\$1/", "derive/^([zcs])([aeiou])/\$1h\$2/")),
         "ch_c" to ("模糊音 ch/c" to listOf()),
         "sh_s" to ("模糊音 sh/s" to listOf()),
-        "n_l" to ("模糊音 n/l" to listOf("derive/^n([aeiou])/l$1/", "derive/^l([aeiou])/n$1/")),
-        "l_r" to ("模糊音 l/r" to listOf("derive/^l([aeiou])/r$1/", "derive/^r([aeiou])/l$1/")),
-        "f_h" to ("模糊音 f/h" to listOf("derive/^f([aeiou])/h$1/", "derive/^h([aeiou])/f$1/")),
+        "n_l" to ("模糊音 n/l" to listOf("derive/^n([aeiou])/l\$1/", "derive/^l([aeiou])/n\$1/")),
+        "l_r" to ("模糊音 l/r" to listOf("derive/^l([aeiou])/r\$1/", "derive/^r([aeiou])/l\$1/")),
+        "f_h" to ("模糊音 f/h" to listOf("derive/^f([aeiou])/h\$1/", "derive/^h([aeiou])/f\$1/")),
         "an_ang" to ("模糊音 an/ang" to listOf("derive/ang$/an/", "derive/an$/ang/")),
         "en_eng" to ("模糊音 en/eng" to listOf("derive/eng$/en/", "derive/en$/eng/")),
         "in_ing" to ("模糊音 in/ing" to listOf("derive/ing$/in/", "derive/in$/ing/")),
@@ -40,8 +40,8 @@ object FuzzyPinyin {
                 .flatMap { it.value.second.ifEmpty { GROUPS[it.key]!!.second } }
                 .toMutableList()
             // zh/z、ch/c、sh/s 三组共用 zh 类规则，这里显式补全
-            if (isEnabled(context, "ch_c")) rules += listOf("derive/^([zcs])h/$1/", "derive/^([zcs])([aeiou])/$1h$2/")
-            if (isEnabled(context, "sh_s")) rules += listOf("derive/^([zcs])h/$1/", "derive/^([zcs])([aeiou])/$1h$2/")
+            if (isEnabled(context, "ch_c")) rules += listOf("derive/^([zcs])h/\$1/", "derive/^([zcs])([aeiou])/\$1h\$2/")
+            if (isEnabled(context, "sh_s")) rules += listOf("derive/^([zcs])h/\$1/", "derive/^([zcs])([aeiou])/\$1h\$2/")
             if (rules.isEmpty()) {
                 patch.delete()
             } else {
